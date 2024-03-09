a6913658386c8444f942e91f1345d20b016d2e73
Mark-Waweru <markwawerumwai@gmail.com>
2024-03-08 16:20:27 -0800

    solutions for task 0-5

---
+++ README.md
@@ -0,0 +1,21 @@
+AirBnb Clone Website
+====================
+
+1.The Console
+=============
+The first step of this project is to create a console which is a simple
+command interpreter shell for
+1. Create your data model
+2. Manage (create, update, destroy, etc) objects via a console / command
+interpreter
+3. Store and persist objects to a file (JSON file)
+
+How to use the command Interpreter(Console)
+===========================================
+- The command Interpreter is started by running the console.py script.
+- After starting the interpreter, you can write the command <help> to see the
+available commands and if you want to know what each command does you just type 
+the command <help> <command>.
+Example:
+$ ./console.py
+(hbnb) help CreateObj
---
+++ models/base_model.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+'''This module has a class BaseModel that defines all common
+attributes/methods for other classes'''
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+    '''Defines all common attributes/methods for other classes
+
+    Attributes:
+        id (str): A unique id for an instance(object)
+        created_at (str): The time the instance(object) was created
+        updated_at (str): The time the instance(object) was updated
+    '''
+    def __init__(self, *args, **kwargs):
+        '''Initializes the instances with this id, created_at, updated_at
+        attributes
+        OR
+        Creates an instance form a dictionary passed with the **kwargs
+
+        Args:
+            *args (object): Any data type object(unlimited)
+            **kwargs (dictionery): Unlimited number of  dictionarys
+        '''
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "updated_at" or key == "created_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+            
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        '''print: [<class name>] (<self.id>) <self.__dict__>
+
+        Returns:
+            [<class name>] (<self.id>) <self.__dict__>
+        '''
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__
+                )
+
+    def save(self):
+        '''updates the public instance attribute updated_at with the current
+        datetime'''
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        '''returns a dictionary containing all keys/values of __dict__
+        of the instance'''
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
